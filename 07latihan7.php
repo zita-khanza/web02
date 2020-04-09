a1112b9bc1c594a7f5e75bc11d169ba6b4bea103
zita-khanza <zitakhanza03@gmail.com>
2020-04-09 21:40:05 +0700

    07latihan5

---
+++ 07latihan7.php
@@ -0,0 +1,15 @@
+<HTML> 
+<HEAD> 
+<TITLE> Penggunaan Split </TITLE> 
+</HEAD> 
+<BODY> 
+<?php 
+$tanggal = "17-05-2010"; 
+list($hari, $bulan, $tahun) = split("-", $tanggal); 
+echo "Hari = $hari"; 
+echo "<br />"; 
+echo "Bulan = $bulan"; 
+echo "<br />"; 
+echo "Tahun = $tahun"; ?> 
+</BODY> 
+</HTML>
